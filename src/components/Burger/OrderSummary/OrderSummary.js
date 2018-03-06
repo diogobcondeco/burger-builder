1c6ba74d1caccf3e089820c0dd72dae9f79a5edb
Diogo Condêço <diogobcondeco@gmail.com>
2018-03-06 17:13:17 +0000

    Adding the Price to the Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}€</strong></p>
             <p>Continue to Checkout?</p>
             <Button
                 btnType='Danger'
