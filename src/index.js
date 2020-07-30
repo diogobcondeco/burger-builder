1fc0bb94860f42c318a65bfa9740e93a687f154b
Diogo Condêço <diogobcondeco@gmail.com>
2020-07-30 20:03:28 +0100

    basic redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+const store = createStore();
 
 const app = (
-    <div>
-        <BrowserRouter BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
             <App />
         </BrowserRouter>
-    </div>
+    </Provider>
 
 )
 
