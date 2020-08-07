7c0ec212cb7a45693f4eeee943226a3a39778386
Diogo Condêço <diogobcondeco@gmail.com>
2020-08-07 22:06:51 +0100

    redirecting the user to the checkout page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -56,6 +56,12 @@ class Auth extends Component {
         this.setState({ controls: updatedControls })
     }
 
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/'{
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     checkValidity(value, rules) {
         let isValid = true;
         if (!rules) {
@@ -133,7 +139,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if (this.props.isAuthenticated) {
-            authRedirect = <Redirect />
+            authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
@@ -156,13 +162,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticated: state.auth.token
+        isAuthenticated: state.auth.token,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
