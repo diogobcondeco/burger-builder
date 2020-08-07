fb86f4a15615a36bbc70aec23551787cb1a706a4
Diogo Condêço <diogobcondeco@gmail.com>
2020-08-07 16:14:22 +0100

    logging users out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -28,11 +28,19 @@ const authFail = (state, action) => {
     });
 }
 
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null
+    })
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionsTypes.AUTH_START: return authStart(state, action);
         case actionsTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionsTypes.AUTH_FAIL: return authFail(state, action);
+        case actionsTypes.AUTH_LOGOUT: return authLogout(state, action);
         default: return state;
     }
 }
