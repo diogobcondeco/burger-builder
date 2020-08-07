21b32765004943938712ef6ec69cb89c0fcaffc0
Diogo Condêço <diogobcondeco@gmail.com>
2020-08-07 15:26:18 +0100

    adding sign in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCg1moIzFvzXvuW05YRt_-eC00Otzg2ffk', authData)
+        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCg1moIzFvzXvuW05YRt_-eC00Otzg2ffk";
+        if (!isSignup) {
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCg1moIzFvzXvuW05YRt_-eC00Otzg2ffk";
+        }
+        axios.post(url, authData)
             .then(response => {
                 dispatch(authSuccess(response.data))
             })
